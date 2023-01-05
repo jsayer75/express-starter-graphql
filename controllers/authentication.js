460ffaa0cf4b9c2076fe59c66b235a13b4a0dd4c
James Sayer <james.sayer760@gmail.com>
2023-01-04 19:46:26 -0500

    add hashPassword & comparehash and generateToken & authenticateToken

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,46 @@
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-// access config var
-process.env.TOKEN_SECRET;
+function hashPassword(plaintextPassword) {
+  bcrypt.genSalt(10, async (err, salt) => {
+    bcrypt.hash(plaintextPassword, salt, (err, hash) => {
+      // TODO: Store hash in the database
+      console.log({ hash });
+    });
+  });
+}
+// compare password
+async function comparePassword(plaintextPassword, hash) {
+  const result = await bcrypt.compare(plaintextPassword, hash).catch((e) => {
+    throw new Error(e);
+  });
+  return result;
+}
+
+function generateAccessToken(username) {
+  return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: "1m" });
+}
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    console.log(err);
+
+    if (err) return res.sendStatus(403);
+    console.log({ user });
+    req.user = user;
+
+    next();
+  });
+}
+
+module.exports = {
+  hashPassword,
+  comparePassword,
+  generateAccessToken,
+  authenticateToken,
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,34 @@
 var express = require("express");
+const {
+  generateAccessToken,
+  hashPassword,
+  comparePassword,
+} = require("../controllers/authentication");
 var router = express.Router();
 
 /* Signup Request */
 router.post("/", function (req, res, next) {
-  res.send("ok");
+  try {
+    hashPassword(req.body.password);
+    res.json({ message: "OK" });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 /* Signup Request */
-router.post("/signin", function (req, res, next) {
-  res.send("ok");
+router.post("/signin", async function (req, res, next) {
+  // TODO: Get Hash from database for req.body.email
+  // `test` password hash: $2b$10$VAGcBtMcPfbmtyhLrGk0DO7Mlsn5npcHWPkyLSVLb036gA8DI9jlq
+  const hash = "$2b$10$VAGcBtMcPfbmtyhLrGk0DO7Mlsn5npcHWPkyLSVLb036gA8DI9jlq";
+  const passwordVerified = await comparePassword(req.body.password, hash);
+
+  const token = generateAccessToken({
+    email: req.email,
+    password: req.password,
+  });
+
+  res.json({ authToken: token, verified: passwordVerified });
 });
 
 module.exports = router;
